17af26ddeb9303154c0497a4b0d80e6a563d3c19
Mark Nolledo <mark.nolledo@gmail.com>
2024-01-23 09:39:23 +0800

    Added new files; updated index page

--- index.php
+++ index.php
@@ -1,18 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Getting ready</title>
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="assets/images/icons/favicon.ico">
+    <title>Getting ready - Mk N.</title>
 </head>
+
 <body>
-<div class="container-fluid">
-    <div class="d-flex align-items-center justify-content-center h-100">
-        <p class="text-wrap fs-2 fw-bolder">Getting ready.</h4>
+    <div class="container-fluid">
+        <div class="d-flex flex-wrap align-items-center justify-content-center" style="height: 100vh;">
+            <div class="col-12 text-center d-flex flex-wrap flex-col align-items-center justify-content-center">
+                <div class="col-12 text-wrap fs-3 fw-bolder text-secondary">Great things are not tested in production.</div>
+                <div class="col-12 text-wrap fs-6 text-secondary">Constructing stuff in local.</div>
+                <div class="col-12 my-4">
+                    <p class="fs-6 mb-2 text-dark">Let's connect.</p>
+                    <a href="https://github.com/Mespeon" target="_blank" class="btn btn-sm btn-dark" role="button">GitHub</a>
+                    <a href="https://ph.linkedin.com/in/mark-nolledo-0404541a8" type="button" class="btn btn-sm btn-primary linkedin-btn">LinkedIn</a>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="position-absolute bottom-0 end-0">
+                <div class="d-flex flex-wrap align-items-center justify-content-end container-fluid text-secondary">
+                    <p class="fs-6">&copy; <?php echo date('Y'); ?> M.N.</p>
+                </div>
+            </div>
+        </footer>
     </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
 </html>
